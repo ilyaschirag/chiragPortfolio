db6f2c62da9e54e495af23e031ae74084f1d9c24
ilyaschirag <142200329+ilyaschirag@users.noreply.github.com>
2025-11-03 23:49:27 +0530

    all

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // 👈 important
   plugins: [react()],
+  base: '/chiragPortfolio/', // 👈 your GitHub repo name
 })
